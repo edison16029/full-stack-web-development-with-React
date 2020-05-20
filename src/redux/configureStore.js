4421dd610555d7004ce6c968a3db282a7e0646a9
Edison M <edison16029@gmail.com>
2020-05-20 19:57:03 +0530

    React Redux Form Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,13 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createForms } from 'react-redux-form'; 
 import { Dishes} from './dishes';
 import { Comments } from './comments';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
+
 
 
 export const configureStore = () => {
@@ -13,7 +16,10 @@ export const configureStore = () => {
 			dishes : Dishes,
 			comments : Comments,
 			promotions : Promotions,
-			leaders : Leaders
+			leaders : Leaders,
+			...createForms({
+                feedback: InitialFeedback
+            })
 		}),
 		applyMiddleware(thunk,logger)
 	);
