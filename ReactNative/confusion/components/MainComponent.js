0467195430439bece30bb91e3268063a0e86667d
Edison M <edison16029@gmail.com>
2020-05-23 21:13:38 +0530

    React Native - Components Part 2

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
 import MenuComponent from './MenuComponent';
+import DishdetailComponent from './DishdetailComponent';
+import { View } from 'react-native';
 
 class MainComponent extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            dishes : DISHES
+            dishes : DISHES,
+            selectedDishId : null
          };
+    }
+
+    onDishSelect(dishId) {
+        this.setState({selectedDishId : dishId});
     }
     render() {
         return (
-            <MenuComponent dishes={this.state.dishes} />
+            <View>
+                <MenuComponent dishes={this.state.dishes} 
+                    onPress= {(dishId) => this.onDishSelect(dishId)}
+                />
+                <DishdetailComponent dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDishId)[0]} />
+            </View>
+
         );
     }
 }
--- ReactNative/confusion/components/MenuComponent.js
+++ ReactNative/confusion/components/MenuComponent.js
@@ -11,6 +11,7 @@ function MenuComponent(props){
             title = {item.name}
             subtitle = {item.description}
             hideChevron = {true}
+            onPress = {() => props.onPress(item.id)}
             leftAvatar={{ source: require('./images/uthappizza.png')}}
             />
         );
