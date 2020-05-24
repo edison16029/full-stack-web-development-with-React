fb1376ef08730941cda028b2263251843f749036
Edison M <edison16029@gmail.com>
2020-05-24 11:32:36 +0530

    RN Navigation part 2 - Drawer

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import MenuComponent from './MenuComponent';
 import DishdetailComponent from './DishdetailComponent';
+import Home from  './HomeComponent';
 import { View } from 'react-native';
-import { createStackNavigator} from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator} from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
         Menu: { screen: MenuComponent },
@@ -22,6 +23,39 @@ const MenuNavigator = createStackNavigator({
     }
 );
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: 
+      { screen: HomeNavigator,
+        navigationOptions: {
+          title: 'Home',
+          drawerLabel: 'Home'
+        }
+      },
+    Menu: 
+      { screen: MenuNavigator,
+        navigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu'
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
+
 
 class MainComponent extends Component {
     constructor(props) {
@@ -31,7 +65,7 @@ class MainComponent extends Component {
     render() {
         return (
             <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MenuNavigator />
+            <MainNavigator />
         </View>
 
         );
