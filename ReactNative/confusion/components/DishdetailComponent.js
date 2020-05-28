56cac89f354e75b3e9a11567a4672c2296b0605e
Edison M <edison16029@gmail.com>
2020-05-28 17:20:59 +0530

    RN Gestures and Persist

--- ReactNative/confusion/components/DishdetailComponent.js
+++ ReactNative/confusion/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, Modal ,StyleSheet,Button } from 'react-native';
+import { View, Text, Modal ,StyleSheet,Button,Alert,PanResponder } from 'react-native';
 import { Card, Rating, Icon, Input } from 'react-native-elements';
 import { ScrollView, FlatList } from 'react-native-gesture-handler';
 import { connect } from 'react-redux';
@@ -25,9 +25,56 @@ const mapDispatchToProps = dispatch => ({
 
 function RenderDish(props){
     const dish = props.dish;
+
+    const handleViewRef = (ref) => this.view = ref;
+
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+        if ( dx < -200 )
+            return true;
+        else
+            return false;
+    }
+    const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        if ( dx > 200 )
+            return true;
+        else
+            return false;
+    }
+
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log("pan responder end", gestureState);
+            if (recognizeDrag(gestureState))
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                    {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPressFav()}},
+                    ],
+                    { cancelable: false }
+                );
+
+            if (recognizeComment(gestureState))
+                    props.onPressComment();
+                
+            return true;
+        }
+    })
+
     if(dish!=null){
         return(
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+            ref={handleViewRef}
+            {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
                     image={{uri: baseUrl + dish.image}}>
